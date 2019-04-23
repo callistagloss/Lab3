333a77480beaa95e4102de09e1722ceca005490b
callistagloss <49454796+callistagloss@users.noreply.github.com>
2019-04-23 14:42:35 -0400

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+
+        {
+            int firstNumber;
+
+            Console.WriteLine("Enter a number between 1 and 100: ");
+            firstNumber = int.Parse((Console.ReadLine()));
+            if (firstNumber < 100 && firstNumber % 2 != 0)
+            {
+                Console.WriteLine("Odd");
+            }
+            else if (firstNumber >= 2 && firstNumber <= 25 && firstNumber % 2 == 0)
+                Console.WriteLine("Even and less than 25.");
+            else if (firstNumber >= 26 && firstNumber <= 60 && firstNumber % 2 == 0)
+                Console.WriteLine("Even.");
+            else if (firstNumber > 60 && firstNumber <= 100 & firstNumber % 2 == 0)
+                Console.WriteLine("Even.");
+            else if (firstNumber > 60 && firstNumber < 100 && firstNumber % 2 != 0)
+                Console.WriteLine("Odd.");
+            else if (firstNumber > 100 || firstNumber < 1)
+                Console.WriteLine("Please enter a positive number between 1 and 100.");
+            }
+
+         
+        }
+    }
+        
+            
+
+            
+
+            
+   
